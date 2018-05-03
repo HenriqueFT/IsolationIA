dc6fdbd8fb0c99834694ae0da55b6882792c4aac
Gabriel Smith <gabrielsmith@air.local>
2018-05-03 17:21:54 -0300

    add clone method and remove unnecessary constructors

--- src/isolationia/BoardState.java
+++ src/isolationia/BoardState.java
@@ -6,38 +6,21 @@ public class BoardState {
         private int yLength = 8;
         private byte [][] board = new byte [xLength][yLength];
         
-        public BoardState(){ //primeira iniciacao
-            for (int i = 0; i < 6; i++) { //deixei os numeros inteiros soh apra ser um pouco mais rapido
-                for (int j = 0; j < 8; j++) {
-                    board[i][j] = 0;
-                }
-            }
+        public BoardState () { }
+        
+        public BoardState (byte[][] board)
+        {
+            for (int i = 0; i < xLength; i++)
+                System.arraycopy(board[i], 0, this.board[i], 0, yLength);
         }
         
-         public BoardState(Player p1,Player p2){ //primeira iniciacao
-            for (int i = 0; i < xLength; i++) { //deixei os numeros inteiros soh apra ser um pouco mais rapido
-                for (int j = 0; j < yLength; j++) {
-                    board[i][j] = 0;
-                }
-            }
+         public BoardState (Player p1,Player p2)
+         {
             board[p1.positionX][p1.positionY] = p1.getID();
             board[p2.positionX][p2.positionY] = p2.getID();
         }
-        
-        public BoardState(BoardState previous){ //caso precisemos apenas copiar o tabuleiro
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    this.board[i][j]=previous.board[i][j];
-                }
-            }
-        }
-        
-        public BoardState(BoardState previous,byte x,byte y){ //caso precisemos adicionar um novo lugar em menos pacos
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    this.board[i][j]=previous.board[i][j];
-                }
-            }
+         
+        public void occupy(byte x,byte y){
             if(this.board [x][y]!=1){
                 this.board [x][y]=1;
             }else{
@@ -45,23 +28,36 @@ public class BoardState {
             }
         }
          
-         public void occupy(byte x,byte y){
-             if(this.board [x][y]!=1){
-                this.board [x][y]=1;
-            }else{
-                System.err.println("TENTANDO AVANCAR PARA CASA JA OCUPADA");
+        public byte [][] getBoard(){
+            return board;
+        }
+         
+        public int getLengthX(){
+            return xLength;
+        }
+         
+        public int getLengthY(){
+            return yLength;
+        }
+        
+        @Override
+        public BoardState clone ()
+        {
+            return new BoardState(this.board);
+        }
+        
+        @Override
+        public String toString ()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[\n");
+            for (int i = 0; i < xLength; i++) {
+                for (int j = 0; j < yLength; j++) {
+                    sb.append(this.board[i][j]).append(",");
+                }
+                sb.append('\n');
             }
-         }
-         
-         public byte [][] getBoard(){
-             return board;
-         }
-         
-         public int getLengthX(){
-             return xLength;
-         }
-         
-          public int getLengthY(){
-             return yLength;
-         }
+            sb.append(']');
+            return sb.toString();
+        }
 }
--- src/isolationia/IsolationIA.java
+++ src/isolationia/IsolationIA.java
@@ -4,7 +4,12 @@ package isolationia;
 public class IsolationIA {
 
     public static void main(String[] args) {
-        Judge.startGame();
+        //Judge.startGame();
+        
+        BoardState xablau = new BoardState();
+        System.out.println(xablau.toString());
+        System.out.println(xablau.clone().toString());
+        System.out.println(xablau.equals(xablau.clone()));
     }
     
 }
